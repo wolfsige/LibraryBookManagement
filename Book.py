bf322518ebfc5a487a683ccc12cee686e71b5481
Wolf <d.wolftripp@gmail.com>
2025-01-17 13:17:32 -0800

    added Accessors in class, added ability for user imputs, will print users inputs with str method

--- Book.py
+++ Book.py
@@ -1,6 +1,19 @@
 class Book:
-    def __init__(self):
-        self.Title = ""
-        self.Author = ""
-        self.Check_Out_Status = 0
-        self.__str__ = f"This book has a string"
+    def __init__(self, title="", author="", check_out_status=0):
+        self.__Title = title
+        self.__Author = author
+        self.__Check_Out_Status = check_out_status
+
+    def getTitle(self):
+        return self.__Title
+
+    def getAuthor(self):
+        return self.__Author
+
+    def getCheck_out_Status(self):
+        return self.__Check_Out_Status
+
+    def __str__(self):
+        return (f"Title: {self.__Title}\n"
+                f"Author: {self.__Author}\n"
+                f"Checked Out: {'Yes' if self.__Check_Out_Status == 0 else 'No'}")
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import Book
 
 def main():
-    print("Hello")
-    print("Hello")
+    title = input("What is the books Title: ")
+    author = input("What is the books Author: ")
+    check_out = int(input("Enter 0 if the book is available, 1 if the book is checked out: "))
 
+    the_book = Book.Book(title, author, check_out)
+
+    print(the_book.__str__())
 
 main()
