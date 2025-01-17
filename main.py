a6e636146d59e92feaf135f282c9c38e57d09577
Wolf <d.wolftripp@gmail.com>
2025-01-17 14:22:51 -0800

    sends user inputs to a text file to act as storage

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ def main():
     title = input("What is the books Title: ")
     author = input("What is the books Author: ")
     check_out = int(input("Enter 0 if the book is available, 1 if the book is checked out: "))
+    print() #Console Readability
 
     the_book = Book.Book(title, author, check_out)
 
-    print(the_book.__str__())
+    with open("Library.txt", "a") as lib:
+        lib.write(the_book.getTitle())
+        lib.write("\n")
+        lib.write(the_book.getAuthor())
+        lib.write("\n")
+        lib.write(str(the_book.getCheck_out_Status()))
+        lib.write("\n")
+
 
 main()
