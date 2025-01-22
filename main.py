5bf851e1d5f0bb5a2edc2b78b75d532a41aaf35a
Wolf <d.wolftripp@gmail.com>
2025-01-22 10:57:44 -0800

    add docstrings to functions to explain their purpose

--- main.py
+++ main.py
@@ -30,6 +30,19 @@ def main():
         print("Exited Program")
 
 def check_in_book(returning_book):
+    """
+        Searches for a book in the library and updates its check-out status.
+
+        This function reads the library from Library.txt and searches for a book
+        matching the provided title. If found, the book's check_out_status is set
+        to 1 (checked in), and the library file is updated to reflect the change.
+
+        Parameters:
+        returning_book (str): The title of the book to search for in the library.
+
+        Returns:
+        none
+    """
     found_books = read_library()
 
     for x in found_books:
@@ -47,6 +60,19 @@ def check_in_book(returning_book):
             lib.write("\n")
 
 def check_out_book(searching_book):
+    """
+        Searches for a book in the library and updates its check-out status.
+
+        This function reads the library from Library.txt and searches for a book
+        matching the provided title. If found, the book's check-out status is set
+        to 0 (checked out), and the library file is updated to reflect the change.
+
+        Parameters:
+        searching_book (str): The title of the book to search for in the library.
+
+        Returns:
+        none
+    """
     found_books = read_library()
 
     for x in found_books:
@@ -64,6 +90,20 @@ def check_out_book(searching_book):
             lib.write("\n")
 
 def menu():
+    """
+        Prompts the user to enter a number corresponding to the number next to the printed menu option.
+
+        this function has the user input a number to navigate the program.
+        If a number that is not present on the menu, or a value such as a string,
+        is tried the function defaults to exiting the program.
+
+        Parameters:
+        none
+
+        Returns:
+        num: return the user input to be used with a match/case in main()
+    """
+
     print() # Console Readability
     try:
         print("1. Add Book\n"
@@ -78,6 +118,17 @@ def menu():
         print()
 
 def add_book():
+    """
+        Prompts the user to input book details (title and author) and defaults check out status to 1, aka checked in.
+        The inputs (author and title) are both made uppercase and then the_book object is created.
+
+        Parameters:
+        None
+
+        Returns:
+        the_book: returned to be used by write_library()
+    """
+
     title = input("What is the books Title: ").upper()
     author = input("What is the books Author: ").upper()
     check_out = 1
@@ -88,6 +139,18 @@ def add_book():
     return the_book
 
 def show_library (books):
+    """
+        Uses an if statement to check the books parameter. iIf it is an empty list it tells the user they should add
+        a book. If the list is not empty, the user is shown all the books in the library.
+
+        Parameters:
+        books (list): a list of all the read information from Library.txt passed from read_library()
+                    each represented as a string.
+
+        Returns:
+        none
+    """
+
     if len(books) == 0:
         print() # Console Readability
         print("The Library seems empty! Maybe you should add a Book!")
@@ -99,6 +162,19 @@ def show_library (books):
 
 
 def write_library(book_object):
+    """
+        Using the_book object passed from add_book() this function opens the text file and appends
+        with the details of the user inputs from add_book() (title, author, and default check_in_status)
+
+
+        Parameters:
+        book_object (Book): An instance of the Book class, containing the title, author,
+                        and check_out status of a book.
+
+        Returns:
+        none
+    """
+
     with open("Library.txt", "a") as lib:
         lib.write(book_object.getTitle())
         lib.write("\n")
@@ -108,6 +184,17 @@ def write_library(book_object):
         lib.write("\n")
 
 def read_library():
+    """
+        Opens Library.txt to read the file and collect the information (title, author, check_out) putting it
+        into a list.
+
+        Parameters:
+        none
+
+        Returns:
+        all_books: returns the full file of Library.txt as a list which will be used by show_library()
+        """
+
     with open("Library.txt", "r") as r_lib:
         all_books = []
         title = r_lib.readline().rstrip("\n")
